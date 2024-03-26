9ebf222c4f8924482c5691183cd0ae9ab2cd67a4
v-nvtsk <v@nvtsk.ru>
2024-03-27 02:47:22 +0300

    feat(component): add spacer

---
+++ src/components/spacer/spacer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./style.module.css";
+
+export const Spacer = ({ space = 10 }: { space: number }) => (
+  <div
+    className={styles.spacer}
+    style={{ paddingTop: `${space}px`, paddingBottom: `${space}px` }}
+  >
+    <hr />
+  </div>
+);
